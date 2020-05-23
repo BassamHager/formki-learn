82fe4857f2ad006d76fa2faea521da20288771d9
BassamHajar <b2dndr1@gmail.com>
2020-05-23 21:31:44 +0200

    U: TextError.jsx M: update accordingly

--- formik-learn/src/components/YouTubeForm.jsx
+++ formik-learn/src/components/YouTubeForm.jsx
@@ -1,57 +1,61 @@
 import React from "react";
 // libraries
 import { Formik, Form, Field, ErrorMessage } from "formik";
-
-// Formik props
+// components
 import { useFormikProps } from "./FormikProps";
-const { initialValues, validationSchema, onSubmit } = useFormikProps();
+import TextError from "./TextError";
 
-const YouTubeForm = () => (
-  <Formik
-    initialValues={initialValues}
-    validationSchema={validationSchema}
-    onSubmit={onSubmit}
-  >
-    <Form>
-      <div className="form-control">
-        <label htmlFor="name">Name</label>
-        <Field type="text" id="name" name="name" />
-        <ErrorMessage name="name" />
-      </div>
+const YouTubeForm = () => {
+  // formik props
+  const { initialValues, validationSchema, onSubmit } = useFormikProps();
 
-      <div className="form-control">
-        <label htmlFor="email">E-Mail</label>
-        <Field type="email" id="email" name="email" />
-        <ErrorMessage name="email" />
-      </div>
+  return (
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={onSubmit}
+    >
+      <Form>
+        <div className="form-control">
+          <label htmlFor="name">Name</label>
+          <Field type="text" id="name" name="name" />
+          <ErrorMessage name="name" component={TextError} />
+        </div>
 
-      <div className="form-control">
-        <label htmlFor="channel">Channel</label>
-        <Field type="text" id="channel" name="channel" />
-        <ErrorMessage name="channel" />
-      </div>
+        <div className="form-control">
+          <label htmlFor="email">E-Mail</label>
+          <Field type="email" id="email" name="email" />
+          <ErrorMessage name="email" component={TextError} />
+        </div>
 
-      <div className="form-control">
-        <label htmlFor="comments">Comments</label>
-        <Field as="textarea" type="text" id="comments" name="comments" />
-        <ErrorMessage name="channel" />
-      </div>
+        <div className="form-control">
+          <label htmlFor="channel">Channel</label>
+          <Field type="text" id="channel" name="channel" />
+          <ErrorMessage name="channel" component={TextError} />
+        </div>
 
-      <div className="form-control">
-        <label htmlFor="comments">Address</label>
-        <Field name="address">
-          {({ field, meta: { touched, error } }) => (
-            <div>
-              <input type="text" id="address" {...field} />
-              {touched && error && <div>{error}</div>}
-            </div>
-          )}
-        </Field>
-      </div>
+        <div className="form-control">
+          <label htmlFor="comments">Comments</label>
+          <Field as="textarea" type="text" id="comments" name="comments" />
+          {/* <ErrorMessage name="comments" component={TextError} /> */}
+        </div>
 
-      <button>Submit</button>
-    </Form>
-  </Formik>
-);
+        <div className="form-control">
+          <label htmlFor="address">Address</label>
+          <Field name="address">
+            {({ field, meta: { touched, error } }) => (
+              <div>
+                <input type="text" id="address" {...field} />
+                {touched && error && <div className="error">{error}</div>}
+              </div>
+            )}
+          </Field>
+        </div>
+
+        <button>Submit</button>
+      </Form>
+    </Formik>
+  );
+};
 
 export default YouTubeForm;
